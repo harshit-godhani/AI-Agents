38f55a1f5f078c8cce497fdb4af1f0cb4f387134
harshit-godhani <godhaniharshit871@gmail.com>
2025-05-07 14:19:50 +0530

    intial commit

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
+from dotenv import load_dotenv
 
-GROQ_API_KEY = "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"
-GRAPHHOPPER_API_KEY = "c7a2732d-86e2-4a3b-8cad-77b6eb761016"
+load_dotenv()
 
-os.environ["GROQ_API_KEY"] = GROQ_API_KEY
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+GRAPHHOPPER_API_KEY = os.getenv("GRAPHHOPPER_API_KEY")
