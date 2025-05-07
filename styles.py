64d9499ffdd97557f69a67ac0d05af6791e5cf0a
harshit-godhani <godhaniharshit871@gmail.com>
2025-05-07 14:06:59 +0530

    intial commit

--- styles.py
+++ styles.py
@@ -271,7 +271,7 @@ STYLES = """
     }
 
     section[data-testid="stSidebar"] {
-        background-image: url('https://images.unsplash.com/photo-1743558011565-8e1ef02e1e5d?q=80&w=2127&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
+        background-image: url('https://images.unsplash.com/photo-1586073055204-973f5b079e32?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
         background-size: cover;
         background-position: center;
         background-repeat: no-repeat;
